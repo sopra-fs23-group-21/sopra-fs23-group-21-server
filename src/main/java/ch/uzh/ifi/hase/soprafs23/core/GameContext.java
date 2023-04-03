ea2276968c6557185b88953a07be42b9852a3c97
zhi wang <zhi.wang@uzh.ch>
2023-04-03 12:54:26 +0200

    write some comments for core

--- src/main/java/ch/uzh/ifi/hase/soprafs23/core/GameContext.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/core/GameContext.java
@@ -1,4 +1,9 @@
 package ch.uzh.ifi.hase.soprafs23.core;
 
+/**
+ * 游戏牌局上下文：
+ * 初始化牌组，加入房间等等
+ */
 public class GameContext {
+
 }
--- src/main/java/ch/uzh/ifi/hase/soprafs23/core/PokerCombination.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/core/PokerCombination.java
@@ -1,4 +1,7 @@
 package ch.uzh.ifi.hase.soprafs23.core;
-
+/**
+ * 牌组对象
+ * 比如比较牌型，判断是否使用炸弹等等
+ */
 public class PokerCombination {
 }
