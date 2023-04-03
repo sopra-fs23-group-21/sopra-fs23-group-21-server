89188c8a090a4806a14f1bc0f20c627abf523854
zhi wang <zhi.wang@uzh.ch>
2023-04-03 12:34:50 +0200

    create CardsController & write the function of play the cards in controller

--- src/main/java/ch/uzh/ifi/hase/soprafs23/controller/CardsController.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/controller/CardsController.java
@@ -1,4 +1,32 @@
 package ch.uzh.ifi.hase.soprafs23.controller;
 
+import ch.uzh.ifi.hase.soprafs23.controller.base.BaseController;
+import ch.uzh.ifi.hase.soprafs23.core.GameContext;
+import ch.uzh.ifi.hase.soprafs23.core.PokerCombination;
+import ch.uzh.ifi.hase.soprafs23.entity.User;
+import ch.uzh.ifi.hase.soprafs23.model.Result;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Vector;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+@RequestMapping("/cards")
+@RestController
 public class CardsController {
+    public static final Map<Integer,GameContext> GAME_ROOM= new ConcurrentHashMap<>();
+
+    public static final AtomicInteger add= new AtomicInteger();
+    public static final ThreadLocal<User> SESSION_USER = new ThreadLocal<>();
+    public User getUser(){
+        return SESSION_USER.get();
+    }
+    //出牌
+    @PostMapping("/pay/{roomCode}")
+    public Result pay(@PathVariable Integer roomCode, @RequestBody PokerCombination pokerCombination){
+        GameContext gameContext = GAME_ROOM.get(roomCode);
+        gameContext.pay(pokerCombination, getUser().getId());
+        return Result.success();
+    }
 }
