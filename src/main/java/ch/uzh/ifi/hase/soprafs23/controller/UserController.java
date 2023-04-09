dac673e9bc5948a4a63f27525d37fa6a085d2706
UZH-JingCao <jing.cao@uzh.ch>
2023-04-09 10:07:11 +0200

    #41

--- src/main/java/ch/uzh/ifi/hase/soprafs23/controller/UserController.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/controller/UserController.java
@@ -1,10 +1,12 @@
 package ch.uzh.ifi.hase.soprafs23.controller;
 
 import ch.uzh.ifi.hase.soprafs23.entity.User;
+import ch.uzh.ifi.hase.soprafs23.model.Result;
 import ch.uzh.ifi.hase.soprafs23.rest.dto.UserGetDTO;
 import ch.uzh.ifi.hase.soprafs23.rest.dto.UserPostDTO;
 import ch.uzh.ifi.hase.soprafs23.rest.mapper.DTOMapper;
 import ch.uzh.ifi.hase.soprafs23.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -23,7 +25,8 @@ import java.util.Objects;
 @RestController
 public class UserController {
 
-  private final UserService userService;
+  @Autowired
+  private UserService userService;
 
   //UserController(UserService userService) {
 //    this.userService = userService;
@@ -34,6 +37,14 @@ public class UserController {
     public Object register(@RequestBody User user){
         userService.createUser(user);
         return user;
+    }
+
+    //login
+    @PostMapping("/login")
+    public Result login(String username, String password){
+        User login = userService.login(username, password);
+        login.setPassword(null);
+        return Result.success(login);
     }
 
   @GetMapping("/users")
