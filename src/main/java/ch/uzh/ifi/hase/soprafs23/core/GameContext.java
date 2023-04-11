63c471a46488864a04e30aa6888a8b6801e17774
XiongLi <xiong.li@uzh.ch>
2023-04-11 12:02:18 +0800

    implement some functions to play cards, resolve #34

--- src/main/java/ch/uzh/ifi/hase/soprafs23/core/GameContext.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/core/GameContext.java
@@ -1,8 +1,10 @@
 package ch.uzh.ifi.hase.soprafs23.core;
 
 import ch.uzh.ifi.hase.soprafs23.model.Poker;
+import ch.uzh.ifi.hase.soprafs23.model.Result;
 import ch.uzh.ifi.hase.soprafs23.model.UserVo;
 import com.google.common.collect.Lists;
+import com.google.gson.Gson;
 import lombok.Data;
 
 import java.util.List;
@@ -51,20 +53,112 @@ public class GameContext {
      */
     private UserVo user;
 
-    public UserVo getUser(Integer id){
+    /**
+     * 获胜者id
+     */
+    private Integer winner;
+
+    public UserVo getUser(Integer id) {
         for (UserVo userVo : this.userList) {
-            if( userVo.getId().equals(id)){
+            if (userVo.getId().equals(id)) {
                 return userVo;
             }
         }
         throw new RuntimeException("unknown user");
     }
 
-    public void continueGame(Integer userId){
+    //同步
+    public void sync() {
+
+        Gson gson = new Gson();
+        Result<GameContext> success = Result.success(this);
+        WebSocketConfigOne.executor.execute(() -> {
+            //同步数据
+            this.userList.forEach(userVo -> {
+                if (Objects.isNull(userVo.getSession())) {
+                    return;
+                }
+                try {
+                    synchronized (userVo) {
+                        userVo.getSession().getBasicRemote().sendText(gson.toJson(success));
+                    }
+                }
+                catch (Exception e) {
+                    e.printStackTrace();
+                }
+            });
+        });
+    }
+
+    //同步
+    public void sync(Integer id) {
+
+        Gson gson = new Gson();
+        Result<GameContext> success = Result.success(this);
+
+        UserVo user = getUser(id);
+        WebSocketConfigOne.executor.execute(() ->
+                user.getSession().getAsyncRemote().sendText(gson.toJson(success))
+        );
+    }
+
+    /**
+     * 发牌
+     */
+    public void sendCard() {
+
+        this.start = (int) (Math.random() * 3);
+
+        initPokers();
+        //发牌
+        int i = this.start;
+
+        while (pokers.size() != 3) {
+
+            UserVo userVo = userList.get(i);
+            //随机抽取一张底牌
+            int num = (int) (Math.random() * pokers.size());
+
+            Poker poker = pokers.get(num);
+
+            pokers.remove(poker);
+            //加入手牌
+            userVo.getHandCard().add(poker);
+
+            if (++i == 3) {
+                i = 0;
+            }
+        }
+
+        gameStatus = 2;
+
+        this.now = start;
+        //同步牌
+        sync();
+
+    }
+
+    /**
+     * 初始化牌组
+     */
+    public void initPokers() {
+        pokers = Lists.newArrayList(Poker.builder().value(14).build(),
+                Poker.builder().value(15).build());
+        for (int i = 1; i < 14; i++) {
+            for (int j = 1; j < 5; j++) {
+                pokers.add(Poker.builder()
+                        .value(i)
+                        .type(j)
+                        .build());
+            }
+        }
+    }
+
+    public void continueGame(Integer userId) {
         UserVo user = getUser(userId);
         user.setContinue(true);
         List<UserVo> collect = this.userList.stream().filter(UserVo::isContinue).collect(Collectors.toList());
-        if(collect.size()==3){
+        if (collect.size() == 3) {
             sendCard();
         }
     }
@@ -73,13 +167,29 @@ public class GameContext {
      * 出牌 to 未知牌型 出牌错误
      * @param pokerCombination
      */
-    public boolean pay(PokerCombination pokerCombination,Integer userId){
-
+    public boolean pay(PokerCombination pokerCombination, Integer userId) {
+        pokerCombination.initType();
+        if (!this.userList.get(this.now).getId().equals(userId)) {
+            throw new RuntimeException("不是你的回合");
+        }
+        //比较牌型
+        if (Objects.nonNull(this.last) && !this.last.compareTo(pokerCombination)) {
+            throw new RuntimeException("牌型不符合");
+        }
         // 扣减手牌
         this.last = pokerCombination;
         UserVo userVo = getUser(pokerCombination.getUserId());
         userVo.getHandCard().removeAll(pokerCombination.getCard());
-
+        // 判断是否还有手牌
+        if (userVo.getHandCard().size() == 0) {
+            this.winner = this.now;
+            gameOver();
+        }
+        else {
+            // 更换出牌对象
+            next();
+        }
+        sync();
         return true;
     }
 
@@ -87,16 +197,16 @@ public class GameContext {
     /**
      * 跳过
      */
-    public void pass(Integer id){
-        if(!this.userList.get(this.now).getId().equals(id)){
+    public void pass(Integer id) {
+        if (!this.userList.get(this.now).getId().equals(id)) {
             throw new RuntimeException("不是你的回合");
         }
-        if(Objects.isNull(last)){
+        if (Objects.isNull(last)) {
             throw new RuntimeException("先手，不能跳过");
         }
         //下一位
         //如果最后一手牌和下一位是同一个用户 则重置上一首牌
-        if(now == this.userList.indexOf( getUser(this.last.getUserId()))){
+        if (now == this.userList.indexOf(getUser(this.last.getUserId()))) {
             this.last = null;
         }
 
