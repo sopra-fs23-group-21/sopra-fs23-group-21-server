094622a68cd9a361fec8ad45eaced31bc2b9a2b7
weimin3 <bonjour_world@126.com>
2023-04-15 16:31:07 +0200

    resolve #55

--- src/main/java/ch/uzh/ifi/hase/soprafs23/core/PokerCombination.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/core/PokerCombination.java
@@ -233,28 +233,8 @@ public class PokerCombination {
     }
 
     /**
-     * 是否是顺子
+     * whether the combination type is 'Chain'-是否是顺子
      */
-    private boolean  isContinuation(List<Poker> card){
-        if(card.size()<5){
-            return false;
-        }
-        Collections.sort(card);
-        Integer tempNumber = null;
-        for (Poker next : card) {
-            if(Objects.nonNull(tempNumber)){
-                if(tempNumber+1 != next.getValue()){
-                    return false;
-                }
-            }
-            tempNumber = next.getValue();
-        }
-        //13 为牌2
-        return tempNumber <13;
-    }
-    //
-//    DOUBLECONTINUATION(1),
-
 
     /**
     * whether it is 'four and two'是否是四带二
