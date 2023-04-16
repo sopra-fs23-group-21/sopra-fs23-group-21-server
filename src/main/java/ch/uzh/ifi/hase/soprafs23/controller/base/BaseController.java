aa8ec5b256b9469be106072df28a42bb9df8ccbd
XiongLi <xiong.li@uzh.ch>
2023-04-16 20:03:35 +0800

    Get the current player's information, resolve #65

--- src/main/java/ch/uzh/ifi/hase/soprafs23/controller/base/BaseController.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/controller/base/BaseController.java
@@ -3,6 +3,10 @@ package ch.uzh.ifi.hase.soprafs23.controller.base;
 import ch.uzh.ifi.hase.soprafs23.Interceptor.SessionInterceptor;
 import ch.uzh.ifi.hase.soprafs23.entity.User;
 
+
+
+//this is just because I can not change the commit message before, this class is used for corresponding issue
+
 public class BaseController {
     public User getUser(){
         return SessionInterceptor.SESSION_USER.get();
