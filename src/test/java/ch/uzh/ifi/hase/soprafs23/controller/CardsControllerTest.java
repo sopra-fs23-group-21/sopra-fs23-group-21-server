4bc83fb5e2806eccab3f80c273ee275e009eeff2
weimin3 <bonjour_world@126.com>
2023-04-22 22:22:42 +0200

    Update CardsControllerTest.java

--- src/test/java/ch/uzh/ifi/hase/soprafs23/controller/CardsControllerTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs23/controller/CardsControllerTest.java
@@ -1,4 +1,74 @@
 package ch.uzh.ifi.hase.soprafs23.controller;
 
-public class CardsControllerTest {
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+@WebMvcTest(CardsController.class)
+class CardsControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private RoomSync mockRoomSync;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    @Test
+    void testCreateGame1() throws Exception {
+        // Setup
+        // Run the test
+        final MockHttpServletResponse response = mockMvc.perform(post("/cards") .content(gson.toJson(user))
+                        .accept(MediaType.APPLICATION_JSON))
+                .andReturn().getResponse();
+
+        // Verify the results
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
+        verify(mockRoomSync).push();
+    }
 }
