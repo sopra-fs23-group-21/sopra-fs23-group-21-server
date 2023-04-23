f6fbc646cba371ca9c86bf5f00c907c216d5e0f9
weimin3 <bonjour_world@126.com>
2023-04-23 11:28:13 +0200

    resolve #117

---
+++ src/test/java/ch/uzh/ifi/hase/soprafs23/controller/UserControllerTest.java
@@ -0,0 +1,63 @@
+package ch.uzh.ifi.hase.soprafs23.controller;
+
+import ch.uzh.ifi.hase.soprafs23.constant.UserStatus;
+import ch.uzh.ifi.hase.soprafs23.entity.User;
+import ch.uzh.ifi.hase.soprafs23.service.UserService;
+import com.google.gson.Gson;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+
+
+
+
+
+
+
+
+
+/**
+ * UserControllerTest
+ * This is a WebMvcTest which allows to test the UserController i.e. GET/POST
+ * request without actually sending them over the network.
+ * This tests if the UserController works.
+ */
+@WebMvcTest(UserController.class)
+public class UserControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private UserService userService;
+
+    private String token;
+
+    private Gson gson = new Gson();
+
+        User user;
+
+
+    @BeforeEach
+    public void initUser(){
+        user = new User();
+        user.setName("Firstname Lastname");
+        user.setUsername("firstname@lastname");
+        user.setPassword("firstname@123");
+        user.setToken("1");
+        user.setStatus(UserStatus.OFFLINE);
+
+    }
+
+
+
+
+}
