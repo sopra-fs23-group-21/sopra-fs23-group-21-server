79545ee4ce080bd0c61bc1613335f13e8cfbe5fc
weimin3 <bonjour_world@126.com>
2023-05-08 08:46:06 +0200

    resolve #136

--- src/test/java/ch/uzh/ifi/hase/soprafs23/core/GameContextTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs23/core/GameContextTest.java
@@ -36,5 +36,47 @@ class GameContextTest {
         gameContextUnderTest.prepare(user2);
         gameContextUnderTest.initPokers();
     }
-    
+
+
+    @Test
+    @DirtiesContext
+    void testPay() {
+        //准备 READY
+        gameContextUnderTest.continueGame(0);
+        gameContextUnderTest.continueGame(1);
+        gameContextUnderTest.continueGame(2);
+
+        //测试抢地主 CONTEND FOR THE LANDLORD
+        gameContextUnderTest.contend(0, false);
+        gameContextUnderTest.contend(1, false);
+        gameContextUnderTest.contend(2, false);
+
+        // 出牌 DEAL CARDS
+        final PokerCombination pokerCombination = new PokerCombination();
+        pokerCombination.setCombinationType(CombinationType.ONE);
+        pokerCombination.setCard(List.of(Poker.builder()
+                .value(1)
+                .type(1)
+                .build()));
+        pokerCombination.setUserId(0);
+
+
+        // Run the test
+        boolean result = gameContextUnderTest.pay(pokerCombination, gameContextUnderTest.getNow());
+        //pass
+        gameContextUnderTest.pass(gameContextUnderTest.getNow());
+        // Verify the results
+        assertThat(result).isTrue();
+
+        final UserVo resultUser = gameContextUnderTest.exist(0);
+
+        // Verify the results
+        assertNotNull(resultUser);
+
+
+        //退出游戏 EXIT THE GAME
+        result = gameContextUnderTest.quitGame(0);
+        assertThat(result).isTrue();
+    }
+
 }
