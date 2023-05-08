dc0f6c12263b04d4317d28f486337af3cc1236fc
UZH-JingCao <jing.cao@uzh.ch>
2023-05-08 23:35:04 +0200

    Amend the previous commit, close #139

--- src/test/java/ch/uzh/ifi/hase/soprafs23/repository/UserRepositoryIntegrationTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs23/repository/UserRepositoryIntegrationTest.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import ch.uzh.ifi.hase.soprafs23.entity.User;
+import org.junit.jupiter.api.Test;
 
 @DataJpaTest
 public class UserRepositoryIntegrationTest {
