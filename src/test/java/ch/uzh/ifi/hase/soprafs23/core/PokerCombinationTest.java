42791c4f515b8500c527f295408a69ec6969cfca
zhi wang <zhi.wang@uzh.ch>
2023-05-08 11:30:49 +0200

    Add test the use of poker Combination resolve #131

--- src/test/java/ch/uzh/ifi/hase/soprafs23/core/PokerCombinationTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs23/core/PokerCombinationTest.java
@@ -11,6 +11,10 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+
+
+
+
 class PokerCombinationTest {
 
     private PokerCombination pokerCombinationUnderTest;
@@ -21,4 +25,128 @@ class PokerCombinationTest {
         pokerCombinationUnderTest.setCard(List.of(Poker.builder().type(1).value(1).build()));
 
     }
+
+    @Test
+    void testCompareTo() {
+        // Setup
+        PokerCombination o = new PokerCombination();
+        o.setCard(List.of(Poker.builder()
+                .value(2)
+                .build()));
+        // Setup 一张
+        pokerCombinationUnderTest.setCard(Lists.newArrayList(Poker.builder().type(1).value(1).build()));
+        // Run the test
+        pokerCombinationUnderTest.initType();
+
+        assertNotNull(pokerCombinationUnderTest.compareTo(o));
+        // Verify the results
+
+        // Setup 两张
+        pokerCombinationUnderTest.setCard(Lists.newArrayList(Poker.builder().type(1).value(1).build(),Poker.builder().type(1).value(1).build()));
+        o.setCard(Lists.newArrayList(Poker.builder().type(1).value(2).build(),Poker.builder().type(1).value(2).build()));
+        // Run the test
+
+        assertNotNull(pokerCombinationUnderTest.compareTo(o));
+
+
+        // Setup 三张
+        pokerCombinationUnderTest.setCard(Lists.newArrayList(Poker.builder().type(1).value(1).build(),
+                Poker.builder().type(1).value(1).build(),Poker.builder().type(1).value(1).build()));
+        // Run the test
+        o.setCard(Lists.newArrayList(Poker.builder().type(1).value(2).build(),
+                Poker.builder().type(1).value(2).build(),Poker.builder().type(1).value(2).build()));
+        assertNotNull(pokerCombinationUnderTest.compareTo(o));
+
+        // Setup 四张
+        pokerCombinationUnderTest.setCard(Lists.newArrayList(new Poker(1,1),new Poker(1,1),new Poker(1,1),new Poker(1,1)));
+        // Run the test
+        o.setCard(Lists.newArrayList(new Poker(2,1),new Poker(2,1),new Poker(2,1),new Poker(2,1)));
+
+        assertNotNull(pokerCombinationUnderTest.compareTo(o));
+
+
+        // Setup 双王
+        o.setCard(Lists.newArrayList(new Poker(14,1),new Poker(15,1)));
+        // Run the test
+        assertNotNull(pokerCombinationUnderTest.compareTo(o));
+
+        // Setup 四张带两张
+        pokerCombinationUnderTest.setCard(Lists.newArrayList(new Poker(1,1),new Poker(1,1),new Poker(1,1),new Poker(1,1),new Poker(2,1),new Poker(3,1)));
+        // Run the test
+        o.setCard(Lists.newArrayList(new Poker(2,1),new Poker(2,1),new Poker(2,1),new Poker(2,1),new Poker(3,1),new Poker(4,1)));
+
+        assertNotNull(pokerCombinationUnderTest.compareTo(o));
+
+
+        // Setup 三张带一张
+        pokerCombinationUnderTest.setCard(Lists.newArrayList(Poker.builder().type(1).value(1).build(),Poker.builder().type(1).value(2).build(),
+                Poker.builder().type(1).value(1).build(),Poker.builder().type(1).value(1).build()));
+        // Run the test
+        o.setCard(Lists.newArrayList(Poker.builder().type(1).value(2).build(),Poker.builder().type(1).value(2).build(),
+                Poker.builder().type(1).value(2).build(),Poker.builder().type(1).value(3).build()));
+        assertNotNull(pokerCombinationUnderTest.compareTo(o));
+
+        //顺子
+        pokerCombinationUnderTest.setCard(Lists.newArrayList(
+                new Poker(1,1),
+                new Poker(2,1),
+                new Poker(3,1),
+                new Poker(4,1),
+                new Poker(5,1)));
+        // Run the test
+        o.setCard(Lists.newArrayList(
+                new Poker(6,1),
+                new Poker(2,1),
+                new Poker(3,1),
+                new Poker(4,1),
+                new Poker(5,1)));
+
+        assertNotNull(pokerCombinationUnderTest.compareTo(o));
+
+
+        //连队
+        pokerCombinationUnderTest.setCard(Lists.newArrayList(
+                new Poker(1,1),
+                new Poker(1,1),
+                new Poker(2,1),
+                new Poker(2,1),
+                new Poker(3,1),
+                new Poker(3,1)));
+        // Run the test
+        o.setCard(Lists.newArrayList(
+                new Poker(4,1),
+                new Poker(4,1),
+                new Poker(2,1),
+                new Poker(2,1),
+                new Poker(3,1),
+                new Poker(3,1)));
+        assertNotNull(pokerCombinationUnderTest.getCombinationType());
+
+
+        //飞机
+        pokerCombinationUnderTest.setCard(Lists.newArrayList(
+                new Poker(1,1),
+                new Poker(1,1),
+                new Poker(1,1),
+                new Poker(2,1),
+                new Poker(2,1),
+                new Poker(2,1),
+                new Poker(3,1),
+                new Poker(4,1)));
+        // Run the test
+        o.setCard(Lists.newArrayList(
+                new Poker(3,1),
+                new Poker(3,1),
+                new Poker(3,1),
+                new Poker(2,1),
+                new Poker(2,1),
+                new Poker(2,1),
+                new Poker(5,1),
+                new Poker(4,1)));
+        assertNotNull(pokerCombinationUnderTest.compareTo(o));
+        // Run the test
+
+        // Verify the results
+    }
+
 }
