435105b903c0616dded3e8490480f532de4b3404
zhi wang <zhi.wang@uzh.ch>
2023-05-17 16:42:02 +0200

    Add testing to improve the line coverage,fix #135

--- src/main/java/ch/uzh/ifi/hase/soprafs23/entity/User.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/entity/User.java
@@ -21,7 +21,7 @@ import java.io.Serializable;
  */
 @Entity
 @Table(name = "user")
-@Data
+//@Data
 //@Builder
 @NoArgsConstructor
 @AllArgsConstructor
@@ -48,4 +48,51 @@ public class User implements Serializable {
     @Column(nullable = false)
     private UserStatus status;
 
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public UserStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(UserStatus status) {
+        this.status = status;
+    }
 }
